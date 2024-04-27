aafc2a9f1774ae853052025c85e2812314003585
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 10:35:59 +0200

    adaptando el server.js

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(`${__dirname}/divertidafront/dist`));
+app.use(express.static(path.join(__dirname, 'divertidafront', 'dist')));
 
 // Content Security Policy configuration
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; style-src 'self' https://fonts.googleapis.com");
+//   next();
+// });
 
 // Send all requests to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'divertidafront', 'dist', 'index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Default Heroku port
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Servidor iniciado en el puerto 5000');
+});
