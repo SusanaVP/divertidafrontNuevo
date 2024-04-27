565c6b77c951ee25f5cdb823b9596f3f770a8dfe
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 13:18:14 +0200

    browser

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'dist','divertidafront')));
+app.use(express.static(path.join(__dirname, 'dist','divertidafront', 'browser')));
 
 // Send all requests to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist','divertidafront','index.html'));
+  res.sendFile(path.join(__dirname, 'dist','divertidafront','browser','index.html'));
 });
 
 // Default Heroku port
