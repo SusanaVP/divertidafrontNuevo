a847ec068051315fd2b38a4b837b8e141885859c
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 11:30:38 +0200

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'divertidafront', 'dist')));
+app.use(express.static(path.join(__dirname,  'src',  'assets')));
 
-// Content Security Policy configuration
-// Content Security Policy configuration
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico;");
-  next();
-});
-
-
-// Send all requests to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Default Heroku port
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Servidor iniciado en el puerto 5000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
