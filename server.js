d0c6b5f34472b4196338076e8d2a85aa1664d704
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 11:09:25 +0200

    CSP de favicon.ico

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(express.static(path.join(__dirname, 'divertidafront', 'dist')));
 // Content Security Policy configuration
 // Content Security Policy configuration
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico data:;");
+  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico;");
   next();
 });
+
 
 // Send all requests to index.html
 app.get('*', (req, res) => {
