5f872fe493d71cd02157b095b0286af71c95e1d6
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 12:20:42 +0200

    dist

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
-// // Serve static files
-// app.use(express.static(path.join(__dirname, 'divertidafront', 'dist')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'divertidafront', 'dist')));
 
-// // Content Security Policy configuration
-// // Content Security Policy configuration
-// app.use((req, res, next) => {
-//   res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico;");
-//   next();
-// });
+// Content Security Policy configuration
+// Content Security Policy configuration
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico;");
+  next();
+});
 
 
-// // Send all requests to index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'browser', 'index.html'));
-// });
+// Send all requests to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'browser', 'index.html'));
+});
 
 // // Default Heroku port
 // app.listen(process.env.PORT || 5000, () => {
@@ -38,37 +38,3 @@
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
-
-const express = require('express');
-const app = express();
-const path = require('path');
-
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function () {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
