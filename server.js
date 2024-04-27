8e3cdfa5edccfe045d0c4a06191f73197077ca67
Susana Velasco <svelped076@g.educaand.es>
2024-04-27 12:42:30 +0200

    server guardado

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Content Security Policy configuration
-// Content Security Policy configuration
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' https://ddivertida-frontend-64b56329e5d5.herokuapp.com/favicon.ico;");
   next();
